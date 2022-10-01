3838ebc8dc8263b43c6fb3efb36189e18c737f33
drslevi2908@gmail.com <drslevi2908@gmail.com>
2022-10-01 19:33:09 +0700

    repositpry 3

---
+++ repo3/image.js
@@ -0,0 +1,21 @@
+function img1(){
+   let git = document.getElementById('gitbranch')
+
+   git.innerHTML = `
+   
+   <img src="img/branching-model-2.png" alt="img1">
+   <h3>Git Branching Model 2</h3>
+   
+   `
+}
+
+function img2(){
+   let git = document.getElementById('gitbranch')
+
+   git.innerHTML = `
+   
+   <img src="img/branching-model-3.png" alt="img2">
+   <h3>Git Branching Model 2</h3>
+   
+   `
+}
